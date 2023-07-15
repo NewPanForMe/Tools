f478453e5f2a12936923424296271e6ae2523f8c
YuanSaiNewComputer <yuans_work.com>
2023-07-15 12:13:20 +0800

    新增验证码生成函数

---
+++ Ys.Tools/Ys.Tools/MoreTool/RandomTools.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ys.Tools.MoreTool
+{
+    public   class RandomTools
+    {
+        /// <summary>
+        /// 获取6位随机数
+        /// </summary>
+        /// <returns></returns>
+        public static int GenerateSix()
+        {
+            Random generator = new Random();
+            int r = generator.Next(100000, 1000000);
+            return r;
+        }
+
+        /// <summary>
+        /// 获取5位随机数
+        /// </summary>
+        /// <returns></returns>
+        public static int GenerateFive()
+        {
+            Random generator = new Random();
+            int r = generator.Next(10000, 100000);
+            return r;
+        }
+        /// <summary>
+        /// 获取5位随机数
+        /// </summary>
+        /// <returns></returns>
+        public static int GenerateFour()
+        {
+            Random generator = new Random();
+            int r = generator.Next(1000, 10000);
+            return r;
+        }
+
+    }
+}
